b06417500761eae87cd9f4142ac0c994adcc9faa
Jelvibean <yelvyb@yahoo.com>
2018-03-01 21:03:25 -0500

    first commit to js boilerplate project

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './main.css';
+import numeral from 'numeral';
+/* eslint-disable no-console */
+/* eslint-disable no-debugger */
+const courseValue = numeral(3000).format('$0.0.00');
+debugger;
+console.log(`I want to make ${courseValue} a day!!`);
